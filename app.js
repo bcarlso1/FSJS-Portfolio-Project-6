0e6c0d78a6a4f9783f2af42d910ae8d0c293bb0f
bcarlso1 <bcarlso1@gmail.com>
2020-03-30 17:30:30 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -45,12 +45,15 @@ app.get('/about', (req, res) => {
  });
 
 app.use((req, res, next) => {
-  const err= new Error('Aaaagh!');
+  const err = new Error("Aaaagh! It's an error!");
+  console.log('Error encountered!');
+  err.status = 500;
     next(err); 
 });
 
 app.use((err, req, res, next) => {
     res.locals.error = err;
+    res.status(err.status);
     res.render('error');
 });
 
